2d547a2ffcafb4f2db6f2f415d4aebbab8a60940
DarkSideMandrillo <tiziano.meneg@gmail.com>
2024-10-02 21:52:51 +0200

    Aggiunto time

--- EsercizioPerRagazzi/libreria-pynput.py
+++ EsercizioPerRagazzi/libreria-pynput.py
@@ -1,11 +1,12 @@
 from pynput import keyboard
-
+import time
 
 # Funzione chiamata quando un tasto viene premuto
 def on_press(key):
     # try:
     if key.char == "w":
         print("press w")
+        
 
 
 # except AttributeError:
@@ -30,4 +31,21 @@ def start_listener():
         listener.join()
 
 
+def lib_time():
+    # Ottieni l'ora corrente
+    current_time = time.time()
+    print("Epoch Time:", current_time)
+
+    # Formatta il tempo in modo leggibile
+    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print("Formatted Time:", formatted_time)
+
+    # Cronometrare il tempo di esecuzione
+    start = time.perf_counter()
+    time.sleep(1)  # Simula un ritardo di 1 secondo
+    end = time.perf_counter()
+    print(f"Elapsed Time: {end - start:.4f} seconds")
+
+lib_time()
+time.sleep(5)
 start_listener()
